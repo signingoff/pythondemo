b6fd35552a04d45ae04d183eda1c564e7a78f645
signingoff <signingoff@163.com>
2016-07-31 23:33:50 +0800

    'file.py'

--- file.py
+++ file.py
@@ -1,4 +1,7 @@
 import os
+import dbm
+import shelve
+import pickle
 
 def print_file_content():
 	file="C:\Program1.cs"
@@ -16,4 +19,25 @@ def io_api():
 	print(os.path.exists("."))	
 	print(os.path.exists("C:\\"))			
 	
+def testAnydbm():
+	file=dbm.open("movie.txt","c");
+	file["first"]="aaa"
+	file.close()
+	
+def test_pickle():
+	file=open("pickle.txt","wb")
+	t=[1,2,3]
+	pickle.dump(t,file)
+
+def test_shelve():
+	s=shelve.open("shelve.txt","cb");
+	s["first"]="aaaaaaaaaaa"
+	s.close()
+	s=shelve.open("shelve.txt")
+	print(len(s))
+	print(s.keys())
+	print(s['first'])
 io_api()
+testAnydbm()
+test_pickle()
+test_shelve()
